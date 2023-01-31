a0dae802b368d85d9199443e7cb503dc5ffcbc34
Jason Caldwell <jaswrks@o5p.me>
2023-01-31 09:32:04 -0500

    Update.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-declare function merge(obj1:{[x:string|number]:unknown}, obj2:{[x:string|number]:unknown}): {[x:string|number]:unknown};
-declare function patch(obj1:{[x:string|number]:unknown}, obj2:{[x:string|number]:unknown}): {[x:string|number]:unknown};
-declare function update(obj1:{[x:string|number]:unknown}, obj2:{[x:string|number]:unknown}): {[x:string|number]:unknown};
+export function merge(obj1:{[x:string|number]:unknown}, obj2:{[x:string|number]:unknown}): {[x:string|number]:unknown};
+export function patch(obj1:{[x:string|number]:unknown}, obj2:{[x:string|number]:unknown}): {[x:string|number]:unknown};
+export function update(obj1:{[x:string|number]:unknown}, obj2:{[x:string|number]:unknown}): {[x:string|number]:unknown};
 
-declare function addOperation(name: string, callback: (current:{[x:string|number]:unknown}, defaults:{[x:string|number]:unknown}) => boolean):
+export function addOperation(name: string, callback: (current:{[x:string|number]:unknown}, defaults:{[x:string|number]:unknown}) => boolean):
 	 undefined | ((current:{[x:string|number]:unknown}, defaults:{[x:string|number]:unknown}) => boolean);
