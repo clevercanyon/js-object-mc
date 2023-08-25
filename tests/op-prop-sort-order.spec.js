821998139e4bb42a1d876586ca21fba626f485e4
Jason Caldwell <jaswrks@o5p.me>
2023-08-25 09:35:07 -0400

    Project update. [p][robotic]

--- tests/op-prop-sort-order.spec.js
+++ tests/op-prop-sort-order.spec.js
@@ -12,9 +12,7 @@ describe('Test mc.merge() with $propSortOrder', () => {
 			$propSortOrder: ['a', 'b', 'c'],
 		};
 		const merged = mc.merge(obj1, obj2);
-		expect(JSON.stringify(merged, null, 4)).toBe(
-			JSON.stringify({a: 'a', b: 'b', c: 'c'}, null, 4)
-		);
+		expect(JSON.stringify(merged, null, 4)).toBe(JSON.stringify({ a: 'a', b: 'b', c: 'c' }, null, 4));
 	});
 	test('$propSortOrder from README file', () => {
 		const obj1 = {
@@ -47,20 +45,24 @@ describe('Test mc.merge() with $propSortOrder', () => {
 		};
 		const merged = mc.merge(obj1, obj2);
 		expect(JSON.stringify(merged, null, 4)).toBe(
-			JSON.stringify({
-				"a": "a",
-				"prop0": "0",
-				"prop1": ["a", "b", "c"],
-				"prop2": ["a", "b", "c"],
-				"prop3": {
-					"b": "b",
-					"c": {
-						"d": "d"
-					}
+			JSON.stringify(
+				{
+					'a': 'a',
+					'prop0': '0',
+					'prop1': ['a', 'b', 'c'],
+					'prop2': ['a', 'b', 'c'],
+					'prop3': {
+						'b': 'b',
+						'c': {
+							'd': 'd',
+						},
+					},
+					'prop4': '4',
+					'prop00': '00',
 				},
-				"prop4": "4",
-				"prop00": "00"
-			}, null, 4)
+				null,
+				4,
+			),
 		);
 	});
 	test('$ꓺdefault with $ꓺpropSortOrder', () => {
@@ -88,7 +90,7 @@ describe('Test mc.merge() with $propSortOrder', () => {
 				'dependencies': {},
 				'description': 'Another great project by Owner.',
 				'devDependencies': {},
-				'devDependenciesꓺ@owner/skeleton-dev-deps': '*',
+				'devDependenciesꓺ@owner/dev-deps': '*',
 				'engines': {},
 				'exports': null,
 				'files': [],
@@ -124,8 +126,8 @@ describe('Test mc.merge() with $propSortOrder', () => {
 				'os': ['darwin', 'linux'],
 				'engines': {
 					'node': '^19.2.0 || ^19.4.0',
-					'npm': '^8.19.3 || ^9.2.0'
-				}
+					'npm': '^8.19.3 || ^9.2.0',
+				},
 			},
 			'$ꓺunset': ['typings', 'scripts'],
 
@@ -184,89 +186,85 @@ describe('Test mc.merge() with $propSortOrder', () => {
 				'configꓺownerꓺ&ꓺgithubꓺconfigVersion',
 				'configꓺownerꓺ&ꓺgithubꓺenvsVersion',
 
-				'configꓺownerꓺ&ꓺnpmjsꓺconfigVersions'
+				'configꓺownerꓺ&ꓺnpmjsꓺconfigVersions',
 			],
 		};
 		const pkg = mc.merge(obj1, obj2);
 		expect(JSON.stringify(pkg, null, 4)).toBe(
-			JSON.stringify({
-				"private": true,
-				"publishConfig": {
-					"access": "restricted"
-				},
-				"version": "1.0.0",
-				"license": "GPL-3.0",
-				"name": "@owner/project",
-				"description": "Another great project by Owner.",
-				"repository": "https://github.com/owner/project",
-				"homepage": "https://github.com/owner/project#readme",
-				"bugs": "https://github.com/owner/project/issues",
-				"funding": "https://github.com/sponsors/owner",
-				"keywords": [
-					"keyword"
-				],
-				"author": {
-					"name": "Owner",
-					"url": "https://owner.com"
-				},
-				"contributors": [],
-				"type": "module",
-				"files": [],
-				"bin": {},
-				"imports": {},
-				"exports": null,
-				"sideEffects": [],
-				"module": "",
-				"main": "",
-				"browser": "",
-				"unpkg": "",
-				"types": "",
-				"typesVersions": {},
-				"dependencies": {},
-				"peerDependencies": {},
-				"peerDependenciesMeta": {},
-				"optionalDependencies": {},
-				"bundleDependencies": [],
-				"devDependencies": {
-					"@owner/skeleton-dev-deps": "*"
-				},
-				"overrides": {},
-				"workspaces": [],
-				"cpu": [
-					"x64",
-					"arm64"
-				],
-				"os": [
-					"darwin",
-					"linux"
-				],
-				"engines": {
-					"node": "^19.2.0 || ^19.4.0",
-					"npm": "^8.19.3 || ^9.2.0"
-				},
-				"config": {
-					"owner": {
-						"&": {
-							"dotfiles": {
-								"lock": []
+			JSON.stringify(
+				{
+					'private': true,
+					'publishConfig': {
+						'access': 'restricted',
+					},
+					'version': '1.0.0',
+					'license': 'GPL-3.0',
+					'name': '@owner/project',
+					'description': 'Another great project by Owner.',
+					'repository': 'https://github.com/owner/project',
+					'homepage': 'https://github.com/owner/project#readme',
+					'bugs': 'https://github.com/owner/project/issues',
+					'funding': 'https://github.com/sponsors/owner',
+					'keywords': ['keyword'],
+					'author': {
+						'name': 'Owner',
+						'url': 'https://owner.com',
+					},
+					'contributors': [],
+					'type': 'module',
+					'files': [],
+					'bin': {},
+					'imports': {},
+					'exports': null,
+					'sideEffects': [],
+					'module': '',
+					'main': '',
+					'browser': '',
+					'unpkg': '',
+					'types': '',
+					'typesVersions': {},
+					'dependencies': {},
+					'peerDependencies': {},
+					'peerDependenciesMeta': {},
+					'optionalDependencies': {},
+					'bundleDependencies': [],
+					'devDependencies': {
+						'@owner/dev-deps': '*',
+					},
+					'overrides': {},
+					'workspaces': [],
+					'cpu': ['x64', 'arm64'],
+					'os': ['darwin', 'linux'],
+					'engines': {
+						'node': '^19.2.0 || ^19.4.0',
+						'npm': '^8.19.3 || ^9.2.0',
+					},
+					'config': {
+						'owner': {
+							'&': {
+								'dotfiles': {
+									'lock': [],
+								},
+								'build': {
+									'appType': 'cma',
+									'targetEnv': 'any',
+								},
+								'github': {
+									'teams': {},
+									'labels': {},
+									'configVersion': '',
+									'envsVersion': '',
+								},
+								'npmjs': {
+									'configVersions': '',
+								},
 							},
-							"build": {
-								"appType": "cma",
-								"targetEnv": "any"
-							},
-							"github": {
-								"teams": {},
-								"labels": {},
-								"configVersion": "",
-								"envsVersion": ""
-							},
-							"npmjs": {
-								"configVersions": ""
-							}
-						}
-					}
-				}
-			}, null, 4)
+						},
+					},
+				},
+				null,
+				4,
+			),
 		);
 	});
 });
